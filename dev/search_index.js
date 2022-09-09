8aa94bd5f4632055a350e532cb759d4f8a132493
Documenter.jl <documenter@juliadocs.github.io>
2022-09-09 14:58:57 +0000

    build based on 8dffa4a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = OscoNet","category":"page"},{"location":"#OscoNet","page":"Home","title":"OscoNet","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for OscoNet.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [OscoNet]","category":"page"}]
+}
